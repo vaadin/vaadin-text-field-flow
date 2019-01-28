ceb58736466aa6e7769da23b327c03f8456ddad9
Guillermo Alvarez <guillermo@vaadin.com>
2019-01-28 14:01:42 +0200

    Add parser and formatter for NumberField

--- vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/NumberField.java
+++ vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/NumberField.java
@@ -24,6 +24,7 @@ import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.HasValueChangeMode;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.function.SerializableFunction;
 
 /**
  * Server-side component for the {@code vaadin-number-field} element.
@@ -35,6 +36,15 @@ public class NumberField
         implements HasSize, HasValidation, HasValueChangeMode,
         HasPrefixAndSuffix, InputNotifier, KeyNotifier, CompositionNotifier,
         HasAutocomplete, HasAutocapitalize, HasAutocorrect {
+    
+    private static final SerializableFunction<String, Double> PARSER = valueFromClient -> valueFromClient == null
+            || valueFromClient.isEmpty() ? null
+                    : Double.parseDouble(valueFromClient);
+
+    private static final SerializableFunction<Double, String> FORMATTER = valueFromModel -> valueFromModel == null
+            ? ""
+            : valueFromModel.toString();
+
     private ValueChangeMode currentMode;
 
     private boolean isConnectorAttached;
@@ -43,7 +53,7 @@ public class NumberField
      * Constructs an empty {@code NumberField}.
      */
     public NumberField() {
-        super(null, null, false);
+        super(null, null, String.class, PARSER, FORMATTER);
         setValueChangeMode(ValueChangeMode.ON_CHANGE);
     }
 
