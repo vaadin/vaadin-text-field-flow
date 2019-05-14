2294e6e5ec52f6c58141d7ec9ebe8c9da9fa4d7e
Sayo Oladeji <oladejioluwasayo@gmail.com>
2019-05-14 11:29:41 +0300

    Make change events from TextFieldElement bubble
    https://github.com/vaadin/vaadin-components-testbench/pull/74

--- vaadin-text-field-flow-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElement.java
+++ vaadin-text-field-flow-testbench/src/main/java/com/vaadin/flow/component/textfield/testbench/TextFieldElement.java
@@ -21,6 +21,9 @@ import com.vaadin.testbench.HasStringValueProperty;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * A TestBench element representing a <code>&lt;vaadin-text-field&gt;</code>
  * element.
@@ -32,8 +35,34 @@ public class TextFieldElement extends TestBenchElement
     @Override
     public void setValue(String string) {
         HasStringValueProperty.super.setValue(string);
-        dispatchEvent("change");
+        EventsUtil.dispatchEvent(this, "change",
+                Collections.singletonMap("bubbles", true));
         dispatchEvent("blur");
     }
 
+    private static class EventsUtil {
+
+        private EventsUtil() {
+            // Util methods only
+        }
+
+       /**
+        * Dispatches (fires) a custom event of the given type on the element with
+        * the given properties
+        *
+        * @param element
+        *            element that will dispatch the event
+        * @param eventType
+        *            the type of custom event to dispatch
+        * @param customEventInit
+        *            map with properties and values that will be used to initialize
+        *            the event
+        */
+        private static void dispatchEvent(TestBenchElement element, String eventType,
+                Map<String, Object> customEventInit) {
+            element.executeScript(
+                "arguments[0].dispatchEvent(new CustomEvent(arguments[1], arguments[2]));",
+                element, eventType, customEventInit);
+        }
+    }
 }
