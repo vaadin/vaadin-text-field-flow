e845d30b3ec16a70588a6d398d9ba42ab5d9af80
Petter Holmström <petter@vaadin.com>
2019-06-26 10:29:05 +0300

    Manually sync the client side invalid flag if the server side flag has already changed before client side validation was disabled.

---
+++ vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/TextFieldRequiredWithCustomValidationIT.java
@@ -0,0 +1,30 @@
+package com.vaadin.flow.component.textfield.tests;
+
+import com.vaadin.flow.testutil.AbstractComponentIT;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+@TestPath("required-field-custom-validator")
+public class TextFieldRequiredWithCustomValidationIT extends AbstractComponentIT {
+
+    @Test
+    public void foobar() {
+        open();
+
+        TestBenchElement textField = $("vaadin-text-field").first();
+        TestBenchElement input = textField.$("input").first();
+
+        Assert.assertEquals(Boolean.TRUE.toString(), textField.getAttribute("invalid"));
+
+        while (!input.getAttribute("value").isEmpty()) {
+            input.sendKeys(Keys.BACK_SPACE);
+        }
+        input.sendKeys("Valid");
+        input.sendKeys(Keys.ENTER);
+
+        Assert.assertEquals(Boolean.FALSE.toString(), textField.getAttribute("invalid"));
+    }
+}
