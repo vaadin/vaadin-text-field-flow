eb5149c2ec5dbe14bb518349c1c7049708a19bba
Tulio Garcia <tulio@vaadin.com>
2019-08-08 17:19:44 +0300

    Add comments

--- vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/TextFieldValidationSupport.java
+++ vaadin-text-field-flow/src/main/java/com/vaadin/flow/component/textfield/TextFieldValidationSupport.java
@@ -11,6 +11,8 @@ import java.util.regex.Pattern;
 /**
  * Utility class for performing server-side validation of string values in text fields. This is needed because it is
  * possible to circumvent the client side validation constraints using browser development tools.
+ *
+ * @author Vaadin Ltd
  */
 final class TextFieldValidationSupport implements Serializable {
 
@@ -24,22 +26,39 @@ final class TextFieldValidationSupport implements Serializable {
         this.field = field;
     }
 
+    /**
+     * @see GeneratedVaadinTextField#setRequired
+     */
     void setRequired(boolean required) {
         this.required = required;
     }
 
+    /**
+     * @see GeneratedVaadinTextField#setMinlength(double)
+     */
     void setMinLength(Integer minLength) {
         this.minLength = minLength;
     }
 
+    /**
+     * @see GeneratedVaadinTextField#setMaxlength(double)
+     */
     void setMaxLength(Integer maxLength) {
         this.maxLength = maxLength;
     }
 
+    /**
+     * @see GeneratedVaadinTextField#setPattern(String)
+     */
     void setPattern(String pattern) {
         this.pattern = pattern == null || pattern.isEmpty() ? null : Pattern.compile(pattern);
     }
 
+    /**
+     * Test if value is invalid for the field.
+     * @param value value to be tested.
+     * @return <code>true</code> if the value is invalid.
+     */
     boolean isInvalid(String value) {
         if (required && Objects.equals(field.getEmptyValue(), value)) {
             return true;
