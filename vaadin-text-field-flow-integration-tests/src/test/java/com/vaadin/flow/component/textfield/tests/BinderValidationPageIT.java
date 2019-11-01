f35d0239fe3d239f8f81ea15a5111cfd75522459
Pekka Maanpää <pekkamaa@vaadin.com>
2019-11-01 14:23:46 +0200

    Add ITs for all fields

---
+++ vaadin-text-field-flow-integration-tests/src/test/java/com/vaadin/flow/component/textfield/tests/BinderValidationPageIT.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.textfield.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.component.textfield.testbench.BigDecimalFieldElement;
+import com.vaadin.flow.component.textfield.testbench.EmailFieldElement;
+import com.vaadin.flow.component.textfield.testbench.IntegerFieldElement;
+import com.vaadin.flow.component.textfield.testbench.NumberFieldElement;
+import com.vaadin.flow.component.textfield.testbench.PasswordFieldElement;
+import com.vaadin.flow.component.textfield.testbench.TextAreaElement;
+import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
+import com.vaadin.flow.testutil.AbstractComponentIT;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
+
+/**
+ * Verify that when component's internal validation passes, but Binder
+ * validation fails, Binder validation status is effective.
+ */
+@TestPath("binder-validation")
+public class BinderValidationPageIT extends AbstractComponentIT {
+
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Test
+    public void textField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        TextFieldElement field = $(TextFieldElement.class).first();
+        field.setValue("a");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void textArea_internalValidationPass_binderValidationFail_fieldInvalid() {
+        TextAreaElement field = $(TextAreaElement.class).first();
+        field.setValue("a");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void passwordField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        PasswordFieldElement field = $(PasswordFieldElement.class).first();
+        field.setValue("a");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void emailField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        EmailFieldElement field = $(EmailFieldElement.class).first();
+        field.setValue("foo@bar.com");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void bigDecimalField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        BigDecimalFieldElement field = $(BigDecimalFieldElement.class).first();
+        field.setValue("1");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void numberField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        NumberFieldElement field = $(NumberFieldElement.class).first();
+        field.setValue("1");
+        assertInvalid(field);
+    }
+
+    @Test
+    public void integerField_internalValidationPass_binderValidationFail_fieldInvalid() {
+        IntegerFieldElement field = $(IntegerFieldElement.class).first();
+        field.setValue("1");
+        assertInvalid(field);
+    }
+
+    private void assertInvalid(TestBenchElement field) {
+        Assert.assertTrue("Unexpected invalid state",
+                field.getPropertyBoolean("invalid"));
+        Assert.assertEquals(
+                "Expected to have error message configured in the Binder Validator",
+                BinderValidationPage.BINDER_ERROR_MSG,
+                field.getPropertyString("errorMessage"));
+    }
+}
